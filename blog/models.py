e11b980bcad8bf67f5627e2b6bb1325eb141915f
Vladimir <vmedyannikov76@gmail.com>
2021-09-02 16:11:08 +0300

    Absolute url

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -42,6 +43,12 @@ class Post(models.Model):
 
     objects = models.Manager()  # Менеджер по умолчанию
     published = PublishedManager()  # Наш новый менеджер
+
+    def get_absolute_url(self):  # Мы будем использовать этот метод в шаблонах, чтобы получить ссылку на статью
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
 
     class Meta:
         """Позволяет изменить метаданные родительского класса возможно указать сортировку, названия и тп"""
--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 """Создали urls уровня приложения"""
 from django.urls import path
 
-from blog import views
+from blog.views import post_list, post_detail
 
-app_name = 'blog'  # Имя уровня приложения, должно быть уникальным по всему проекту
+app_name = 'blog'  # Пространство имен приложения, должно быть уникальным по всему проекту
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('<int:year>/<int:month>/<int: day>/<slug: post>/', post_detail, name='post_detail'),
+    path('', post_list, name='post_list'),  # name позволит нам обращаться к этому представлению из любой точки проекта
+    # по имени (app_name : name)  в нашем случае ***  urls "blog:post_list"  ***
 ]
