809c9fcd1f78ff5bb28a7a06d1e573c4a79cbb60
Vladimir <vmedyannikov76@gmail.com>
2021-09-02 13:25:33 +0300

    Registering a blog application

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status',)  # список полей на экране
+    list_editable = ('status',)
+    list_filter = ('status', 'created', 'publish', 'author',)  # фильтр
+    search_fields = ('title', 'body',)  # поиск
+    prepopulated_fields = {'slug': ('title',)}  # Для формирования slug автоматически из  title
+    raw_id_fields = ('author',)  # выбор автора по id
+    date_hierarchy = 'publish'  # иерархия вывода
+    ordering = ('status', 'publish')  # сортировка
+
