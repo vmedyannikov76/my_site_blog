e4219c78fcffdece1b9090aec17aea6ea326c92f
Vladimir <vmedyannikov76@gmail.com>
2021-09-02 15:48:48 +0300

    Create views for url requests

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-def index(request):
-    return HttpResponse('здесь будет страница блога')
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):  # Обработчик для отделной статьи принимает на вход 4 аргумента
+    # слаг и дату публикации
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='status_published',
+                             publich__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             )
+    return render(request, 'blog/post/detail.html', {'post': post})
