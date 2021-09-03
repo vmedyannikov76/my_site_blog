7c3e8bcd9a7ab31078e92d5168bcc10c14c74083
Vladimir <vmedyannikov76@gmail.com>
2021-09-03 11:39:01 +0400

    Chapter 1 finished

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 """Создали urls уровня приложения"""
 from django.urls import path
 
-from blog.views import post_list, post_detail
+from blog.views import post_detail, PostListView  # post_list - закоментировали, т.к. используем класс
 
 app_name = 'blog'  # Пространство имен приложения, должно быть уникальным по всему проекту
 
 urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
-    path('', post_list, name='post_list'),  # name позволит нам обращаться к этому представлению из любой точки проекта
+    #  path('', post_list, name='post_list'),  # name позволит нам обращаться к этому представлению из любой точки
+    # проекта
     # по имени (app_name : name)  в нашем случае ***  urls "blog:post_list"  ***
+    path('', PostListView.as_view(), name='post_list'),
 ]
