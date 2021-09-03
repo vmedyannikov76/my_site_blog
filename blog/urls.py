2659ec03f8c334994caa299522657afc68db4b0a
Vladimir <vmedyannikov76@gmail.com>
2021-09-03 09:29:23 +0400

    html full

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from blog.views import post_list, post_detail
 app_name = 'blog'  # Пространство имен приложения, должно быть уникальным по всему проекту
 
 urlpatterns = [
-    path('<int:year>/<int:month>/<int: day>/<slug: post>/', post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
     path('', post_list, name='post_list'),  # name позволит нам обращаться к этому представлению из любой точки проекта
     # по имени (app_name : name)  в нашем случае ***  urls "blog:post_list"  ***
 ]
